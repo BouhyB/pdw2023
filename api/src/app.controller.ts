485cc9cb24533d05cb163c52cd68c7c6a3ae48dd
BouhyB <113918757+BouhyB@users.noreply.github.com>
2023-09-18 19:44:32 +0200

    exception

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {ApiCodeResponse, ApiResponse} from '@common/api';
+import {TestException} from './app.exception';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): void {
+    throw new TestException();
   }
 }
---
+++ api/src/common/api/data/api.response.ts
@@ -0,0 +1,7 @@
+import {ApiCodeResponse} from './enum/api-code.response';
+
+export interface ApiResponse{
+    result: boolean;
+    code: ApiCodeResponse;
+    data: any;
+}
