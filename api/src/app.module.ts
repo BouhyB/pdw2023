78c0f298f83ae53d0c53b83b2fa67d3b4e0e1c6a
BouhyB <113918757+BouhyB@users.noreply.github.com>
2023-09-25 19:44:43 +0200

    config db

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {configManager} from '@common/config/';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(configManager.getTypeOrmConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/common/config/index.ts
+++ api/src/common/config/index.ts
@@ -1,1 +1,3 @@
 export * from './exception/index'
+export * from './enum/index'
+export * from './config.manager'
