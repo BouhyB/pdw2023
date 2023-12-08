39309dddc17701e3787bd1776bf77ece83838ab4
BouhyB <113918757+BouhyB@users.noreply.github.com>
2023-12-08 16:25:14 +0100

    signaux

--- app/src/app/dashboard/feature/member/page/member-detail-page/member-detail-page.component.ts
+++ app/src/app/dashboard/feature/member/page/member-detail-page/member-detail-page.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, computed, effect, Input, Signal, signal, WritableSignal} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {delay, of, tap} from 'rxjs';
 
 @Component({
   selector: 'app-member-detail-page',
@@ -10,4 +11,21 @@ import { CommonModule } from '@angular/common';
 })
 export class MemberDetailPageComponent{
   @Input() id!:string;
+
+  member: WritableSignal<string> = signal('no body');
+
+  memberFirstNameLastname: Signal<string> = computed(() => `fake example of data extraction from object ${this.member()}`)
+
+  memberEffect = effect(() => console.log(`Mon effect sur le member : ${this.member()}`));
+  memberFirstNameLastNameEffect = effect(() => console.log(`Mon effect sur le memberFirstNameLastname : ${this.memberFirstNameLastname()}`));
+
+  ngOnInit(): void {
+    this.getData();
+  }
+  getData(): void {
+    of(this.member()).pipe(
+      delay(5000),
+      tap(() => this.member.set('Nicolas'))
+    ).subscribe();
+  }
 }
